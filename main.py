1f610494f609f38f613fbe1960c55945a3207ae6
Dorwin <dorwinzheng@outlook.com>
2023-05-18 12:32:23 +0200

    bg

--- main.py
+++ main.py
@@ -25,11 +25,25 @@ FPS = 60
 GRAVITY = 0.75
 LOWER_FLOOR = 500
 TILE_SIZE = 40
+TILE_TYPES = 21
+bg_scroll = 0
 
 # Define player actions variable
 move_left = False
 move_right = False
 shoot = False
+
+#load images
+pine1_img = pygame.image.load('img/Background/pine1.png').convert_alpha()
+pine2_img = pygame.image.load('img/Background/pine2.png').convert_alpha()
+mountain_img = pygame.image.load('img/Background/mountain.png').convert_alpha()
+sky_img = pygame.image.load('img/Background/sky_cloud.png').convert_alpha()
+#store tiles in a list
+img_list = []
+for x in range(TILE_TYPES):
+	img = pygame.image.load(f'img/Tile/{x}.png')
+	img = pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
+	img_list.append(img)
 
 # Define bullet
 bullet_img = pygame.image.load('img/icons/bullet.png').convert_alpha()
@@ -47,7 +61,7 @@ item_boxes = {
 }
 
 # Define colours
-BG = (144, 201, 120)
+BG = (255, 201, 120)
 White = (255, 255, 255)
 
 font = pygame.font.SysFont('Time new roman', 30)
@@ -62,6 +76,12 @@ def draw_text(text, font, text_color, x, y):
 
 def draw_bg():
     screen.fill(BG)
+    width = sky_img.get_width()
+    for x in range(5):
+        screen.blit(sky_img, ((x * width) - bg_scroll * 0.5, 0))
+        screen.blit(mountain_img, ((x * width) - bg_scroll * 0.6, SCREEN_HEIGHT - mountain_img.get_height() - 300))
+        screen.blit(pine1_img, ((x * width) - bg_scroll * 0.7, SCREEN_HEIGHT - pine1_img.get_height() - 150))
+        screen.blit(pine2_img, ((x * width) - bg_scroll * 0.8, SCREEN_HEIGHT - pine2_img.get_height()))
     for terrain in All_terrain:
         pygame.draw.rect(screen, White, pygame.Rect((terrain.x-camera_offsetX), (terrain.y-camera_offsetY), terrain.width, terrain.height))
     #pygame.draw.rect(screen, White, ground_platform)
@@ -307,7 +327,7 @@ class Bullet(pygame.sprite.Sprite):
 
 
 
-upper_platform = pygame.Rect(1000, 300, 150, 300)
+upper_platform = pygame.Rect(1000, 400, 150, 100)
 ground_platform = pygame.Rect((-100, LOWER_FLOOR-5), (SCREEN_WIDTH+100, 10))
 second_platform = pygame.Rect((700, 450), (300, 300))
 
@@ -318,9 +338,9 @@ bullet_group = pygame.sprite.Group()
 item_box_group = pygame.sprite.Group()
 
 
-item_box = ItemBox('Health', 100, 260)
+item_box = ItemBox('Health', 100,450 )
 item_box_group.add(item_box)
-item_box = ItemBox('Ammo', 400, 260)
+item_box = ItemBox('Ammo', 400, 450)
 item_box_group.add(item_box)
 
 
